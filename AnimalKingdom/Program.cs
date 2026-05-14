adda9eda1eee5011678363106aab5245923b979f
Isis <mejaikatwt@gmail.com>
2026-05-14 15:53:10 +0100

    commit intermediário

--- AnimalKingdom/Program.cs
+++ AnimalKingdom/Program.cs
@@ -1,41 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace AnimalKingdom
 {
+    // Alternative Program class if you don't want to program your own
     public class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            Animal [] animals = new Animal[10];
-            Random random = new Random();
-            for(int i = 0; i < animals.Length; i++)
+            const int n = 10;
+            Animal[] animals = new Animal[n];
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
             {
-                 int anType = random.Next(1, 5);
-                if(anType == 1)
+                switch (rnd.Next(4))
                 {
-                    animals[i] = new Bat();
-                }
-                if(anType == 2)
-                {
-                    animals[i] = new Cat();
-                }
-                if(anType == 1)
-                {
-                    animals[i] = new Dog();
-                }
-                if(anType == 1)
-                {
-                    animals[i] = new Bee();
+                    case 0:
+                        animals[i] = new Bat();
+                        break;
+                    case 1:
+                        animals[i] = new Bee();
+                        break;
+                    case 2:
+                        animals[i] = new Cat();
+                        break;
+                    case 3:
+                        animals[i] = new Dog();
+                        break;
+                    default:
+                        // This should NEVER happen
+                        throw new ArgumentOutOfRangeException("Unknown animal!");
                 }
             }
-            foreach(Animal a in animals)
+
+            foreach (Animal a in animals)
             {
-               Console.WriteLine(a.Sound());
-               if(a is ICanFly)
-                {
-                    Console.WriteLine()
-                }
+                Console.Write($"{a.GetType().Name}{a.Sound()}");
+                if (a is IMammal)
+                    Console.Write(
+                        $"\t| No.Nipples = {(a as IMammal).NumberOfNipples}");
+                if (a is ICanFly)
+                    Console.Write(
+                        $"\t| No.Wings = {(a as ICanFly).NumberOfWings}");
+                Console.WriteLine();
             }
         }
     }
